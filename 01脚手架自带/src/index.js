50b1ac149dc4eff7afc2b87d708fdbc7a5acdb4e
mq <1596684751@qq.com>
2025-07-22 10:10:53 +0800

    helloReact

--- 01脚手架自带/src/index.js
+++ 01脚手架自带/src/index.js
@@ -1,13 +1,14 @@
-// 引入react核心库
 import React from 'react';
-// 引入ReactDOM
-// 18版本之前引入：import ReactDOM from 'react-dom';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+reportWebVitals();
